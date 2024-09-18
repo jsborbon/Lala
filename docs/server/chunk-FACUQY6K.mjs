4662d52b737e3427093375be5e6c647ad59971b8
Mariajose <you@example.com>
2024-09-17 21:17:40 -0500

    Se crea el build de la aplicación

---
+++ docs/server/chunk-FACUQY6K.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-WRO5WEVO.mjs";import"./chunk-5XUXGTUW.mjs";export default a();
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2ED5GMTR.mjs";import"./chunk-PGJY4ZLN.mjs";import"./chunk-DATEURA7.mjs";import"./chunk-WRO5WEVO.mjs";import"./chunk-RWBLEACG.mjs";import"./chunk-RDXKAYHL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
